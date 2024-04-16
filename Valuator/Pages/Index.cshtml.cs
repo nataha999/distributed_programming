8647005b25ceef35ef583dc99ba97a147184051b
nataha <SNatawaN1@gmail.com>
2024-04-16 10:58:46 +0300

    Исправила GetSimilarity и добавила проверку на пустую строку

--- Valuator/Pages/Index.cshtml.cs
+++ Valuator/Pages/Index.cshtml.cs
@@ -22,6 +22,9 @@ public class IndexModel : PageModel
 
     private static string GetRank(string text)
     {
+        if (String.IsNullOrEmpty(text))
+            return "0";
+
         double nonalphaCounter = 0;
 
         foreach (var ch in text) {
@@ -34,7 +37,7 @@ public class IndexModel : PageModel
         return Convert.ToString(nonalphaCounter / text.Length);
     }
 
-    private int GetSimilarity(string text, string key)
+    private int GetSimilarity(string text)
     {
         var keys = _storage.GetKeys();
         string textPrefix = "TEXT-";
@@ -50,13 +53,16 @@ public class IndexModel : PageModel
 
     public IActionResult OnPost(string text)
     {
+        if (string.IsNullOrEmpty(text))
+            Redirect($"index");
+
         _logger.LogDebug(text);
 
         string id = Guid.NewGuid().ToString();
 
         string similarityKey = "SIMILARITY-" + id;
         //TODO: посчитать similarity и сохранить в БД по ключу similarityKey
-        int similarity = GetSimilarity(text, id);
+        int similarity = GetSimilarity(text);
         _storage.Set(similarityKey, similarity.ToString());
 
         string textKey = "TEXT-" + id;
